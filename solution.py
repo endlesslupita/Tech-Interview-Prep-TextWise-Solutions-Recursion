35084ad15693b298f172c3b9f3057624ab18b6f3
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-15 15:37:57 -0700

    First attempt at solution

---
+++ solution.py
@@ -0,0 +1,18 @@
+string to array?
+def reverse_string():
+# Reverse string using naive method
+    right = len.string
+    while right >= 0:
+        left = 0
+        string[right]
+        new_string.len(right)
+        new_string[left] = string[right]
+        left += 1
+        right -= 1
+
+    return new_string
+
+
+if __name__ == '__main__':
+
+
