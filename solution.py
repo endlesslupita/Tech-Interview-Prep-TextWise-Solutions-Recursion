48108b8abacf758f16f449362f4a5e22e4200f5d
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-15 19:58:26 -0700

    Bug fixes based on clues from Claude Code

--- solution.py
+++ solution.py
@@ -1,9 +1,10 @@
-string to array?
-def reverse_string():
+def reverse_string(string):
 # Reverse string using naive method
-    right = len.string
+    
+    right = len(string) - 1
+    new_string = [''] * right
+    left = 0
     while right >= 0:
-        left = 0
         string[right]
         new_string.len(right)
         new_string[left] = string[right]
@@ -19,6 +20,9 @@ def reverse_string_recursion(string):
     else:
         string = reverse_string_recursion(string[1:]) + string[0]
         return string
+
 if __name__ == '__main__':
+    print(reverse_string('Hello World!'))
+    print(reverse_string_recursion('Hello World'))
 
 
